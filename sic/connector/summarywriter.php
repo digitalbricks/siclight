087037680a35d9c24f3d1651c0331de6e83be28a
digitalbricks <mail@andreherdling.de>
2018-07-04 21:40:35 +0200

    summary writer function

--- sic/connector/summarywriter.php
+++ sic/connector/summarywriter.php
@@ -7,4 +7,58 @@ require_once '../includes/functions.php';
 // getting data sent via json POST to this file
 $data = json_decode(file_get_contents('php://input'), true);
 
+
+
+$targetFile = '../history/_summary-latest.csv';
+
+// delete old file
+if (file_exists($targetFile)) { 
+    unlink($targetFile);
+}
+
+// create file and open if not already in place
+$fh = fopen($targetFile, 'w');
+
+// create table header in CSV
+fputcsv($fh,array('System','Sys Ver','PHP Ver','Sat Ver', 'Date', 'Time'));
+
+// - fallbacks
+$sys_ver = "n/a";
+$php_ver = "n/a";
+$sat_ver = "n/a";
+$sys = "n/a";
+
+foreach ($data as $item){
+    
+    $site_name = $item['site_name'];
+    
+    // getting configuration for this site
+    $config = $sites[$site_name];
+
+    // - overwrite fallbacks if there is data
+    if(isset($item['sys_ver']) AND $item['sys_ver']!=''){
+        $sys_ver = $item['sys_ver'];
+    };
+    if(isset($item['php_ver']) AND $item['php_ver']!=''){
+        $php_ver = $item['php_ver'];
+    };
+    if(isset($item['sat_ver']) AND $item['sat_ver']!=''){
+        $sat_ver = $item['sat_ver'];
+    };
+    if(isset($item['sys']) AND $item['sys']!=''){
+        $sys = $item['sys'];
+    } else {
+        $sys = $config['sys'];
+    };
+
+    // write data to CSV;
+    fputcsv($fh,array($sys ,$sys_ver, $php_ver, $sat_ver, date('d.m.Y'), date('H:i:s')));
+
+}
+
+// close file handle
+fclose($fh);
+
+
+
 var_dump($data);
